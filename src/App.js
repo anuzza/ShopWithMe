5f9f90832e21523f9c89733e934b4c455aec7398
anuzza <anuja3580@gmail.com>
2022-07-03 23:49:38 -0500

    using useEffect to send data to database

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
 
   const show= useSelector(state=> state.cartBtn.showCart);
+
+  const cart = useSelector(state=>state.cartItem)
+
+  useEffect(()=>{
+    fetch('https://react-http-76fdb-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',//overwrite the existing data
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
